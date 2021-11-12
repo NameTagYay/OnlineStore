4a88517f8ff6e1db7a9e645dd53f0f829365296c
pierce.aidyn <pierceaidyn@gmail.com>
2021-11-12 10:08:58 -0600

    Creating the project. Adding a README added a bunch of things, and I am getting ready to start the main and other classes.

--- src/Customer.java
+++ src/Customer.java
@@ -1,75 +1,60 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-
 public class Customer {
     private String username, password, name, address;
     private ArrayList<Integer> pastOrders;
     private LocalDate dob;
     private double distance;
-
-    public Customer(String username, String password, String name, String address, ArrayList<Integer> pastOrders, LocalDate dob, double distance) {
+    public Customer(String username, String password, String name, String address, LocalDate dob) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.address = address;
-        this.pastOrders = pastOrders;
         this.dob = dob;
-        this.distance = distance;
+        pastOrders = new ArrayList<>();
+        distance = getDistance(address);
     }
-
+    public void addOrder(Integer o){
+        pastOrders.add(o);
+    }
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public ArrayList<Integer> getPastOrders() {
         return pastOrders;
     }
-
-    public void setPastOrders(ArrayList<Integer> pastOrders) {
-        this.pastOrders = pastOrders;
-    }
-
     public LocalDate getDob() {
         return dob;
     }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
+    private double getDistance(String address) {
+        return 0;
     }
 
-    public double getDistance() {
-        return distance;
+    public int getDistance(){
+        return (int)distance;
     }
 
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
+
+
 }
